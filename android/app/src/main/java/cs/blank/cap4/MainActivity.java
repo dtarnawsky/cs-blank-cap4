9aff8f882a52c107c050f54f2260e3ebd31be028
dtarnawsky <damian@ionic.io>
2023-05-02 06:54:15 -0700

    Initial Commit

---
+++ android/app/src/main/java/cs/blank/cap4/MainActivity.java
@@ -0,0 +1,5 @@
+package cs.blank.cap4;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'cs.blank.cap4',
+  appName: 'cs-blank-cap4',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
